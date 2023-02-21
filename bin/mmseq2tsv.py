60ac870bbe959b08560c6e1685d59995f8f545d4
Kevin Lamkiewicz <kevin.lamkiewicz@uni-jena.de>
2023-02-21 14:43:35 +0100

    pickling mmseqs2 table instead of creating many tsvs

--- bin/mmseq2tsv.py
+++ bin/mmseq2tsv.py
@@ -13,6 +13,7 @@ __author__ = "Kevin Lamkiewicz"
 
 import sys
 import itertools
+import pickle
 
 # This can't happen anymore. If it does, we screwed up!
 if len(sys.argv) != 4:
@@ -71,8 +72,6 @@ with open(blast, 'r') as inputStream:
         if queryStrain == targetStrain:
             continue
 
-
-
         seqSim = currentEntry[-1]
 
         # orientation is needed to encode whether a gene is reversed in a strain
@@ -81,75 +80,84 @@ with open(blast, 'r') as inputStream:
         else:
             orientation = 1    
         #orientation = -1 if int(currentEntry[8]) > int(currentEntry[9]) else 1
-        with open(f"{outputPath}/{queryStrain}-vs-{targetStrain}.tsv", 'a') as outputStream:
-            outputStream.write(f"{queryGene}\t{targetGene}\t{orientation}\t{seqSim}\n")
+        # with open(f"{outputPath}/{queryStrain}-vs-{targetStrain}.tsv", 'a') as outputStream:
+        #     outputStream.write(f"{queryGene}\t{targetGene}\t{orientation}\t{seqSim}\n")
 
         #key = f"{queryID.split('_')[0]}:{targetID.split('_')[0]}"
-        #key = f"{queryID}:{targetID}"
+        key = f"{'_'.join(queryID)}:{'_'.join(targetID)}"
         #stream = outputStreams[key]
         #stream.write(f"{int(queryID.split('_')[1])}\t{int(targetID.split('_')[1])}\t{orientation}\t{seqSim}\n")
         
-        #if key not in blastTable:
-        #    blastTable[key] = [seqSim, orientation]
-exit(0)
+        if key not in blastTable:
+            blastTable[key] = [seqSim, orientation]
+# exit(0)
 #for stream in outputStreams.values():
 #    stream.close()
 
+with open("mmseqs_compressed.pkl", 'wb') as f:
+    pickle.dump(blastTable, f)
+
 #sys.exit(0)
 # for each strain pairwise comparison, write a file with all gene entries
-pairwiseComparisons = []
+#pairwiseComparisons = []
 
-for key, info in blastTable.items():
-    seqSim = info[0]
-    orientation = info[1]
-    # lots of parsing from our hashes, nothing super special
-    queryID = key.split(':')[0]
-    targetID = key.split(':')[1]
-    queryStrain = queryID.split('_')[0]
-    queryGene = int(queryID.split('_')[1])
-    targetStrain = targetID.split('_')[0]
-    targetGene = int(targetID.split('_')[1])
-    with open(f"{outputPath}/{queryStrain}-vs-{targetStrain}.tsv", 'a') as outputStream:
-        outputStream.write(f"{queryGene}\t{targetGene}\t{orientation}\t{seqSim}\n")
+# for key, info in blastTable.items():
+#     print(key, info)
+#     seqSim = info[0]
+#     orientation = info[1]
+#     # lots of parsing from our hashes, nothing super special
+#     queryID = key.split(':')[0].split("_")
+#     targetID = key.split(':')[1].split("_")
+#     #print(queryID)
+#     queryStrain = queryID[0]
+#     queryGene = int(queryID[1])
+#     targetStrain = targetID[0]
+#     targetGene = int(targetID[1])
+#     bitscore = int(seqSim)
+
+    #with open(f"{outputPath}/{queryStrain}-vs-{targetStrain}.tsv", 'a') as outputStream:
+    #    outputStream.write(f"{queryGene}\t{targetGene}\t{orientation}\t{seqSim}\n")
 
 
 
-for strainA in strains.keys():
-    for strainB in strains.keys():
-#        # again, self-hits are ignored
-        if strainA == strainB:
-            continue
-#
-#        # check whether we already have that file
-        if strainA < strainB:
-            first = strainA
-            second = strainB
-        else:
-            first = strainB
-            second = strainA
+# for strainA in strains.keys():
+#     for strainB in strains.keys():
+# #        # again, self-hits are ignored
+#         if strainA == strainB:
+#             continue
+# #
+# #        # check whether we already have that file
+#         if strainA < strainB:
+#             first = strainA
+#             second = strainB
+#         else:
+#             first = strainB
+#             second = strainA
             
-        blastKey = f"{first}:{second}"
-        blastKeyRev = f"{second}:{first}"
-        if blastKey in pairwiseComparisons or blastKeyRev in pairwiseComparisons:
-            continue
-#        
-        pairwiseComparisons.append(blastKey)
-        pairwiseComparisons.append(blastKeyRev)
-#        
-        with open(f"{outputPath}/{first}-vs-{second}.tsv", 'w') as outputStream:
-            for key, info in blastTable.items():
-                seqSim = info[0]
-                orientation = info[1]
-                # lots of parsing from our hashes, nothing super special
-                queryID = key.split(':')[0]
-                targetID = key.split(':')[1]
-                queryStrain = queryID.split('_')[0]
-                queryGene = int(queryID.split('_')[1])
-                targetStrain = targetID.split('_')[0]
-                targetGene = int(targetID.split('_')[1])
-                # if the blast-key is the same as the current strains we are looking at, then we have to write
-                # otherwise simply continue with the next blast hit
-                if strainA == queryStrain and strainB == targetStrain:
-                    outputStream.write(f"{queryGene}\t{targetGene}\t{orientation}\t{seqSim}\n")
+#         blastKey = f"{first}:{second}"
+#         blastKeyRev = f"{second}:{first}"
+#         if blastKey in pairwiseComparisons or blastKeyRev in pairwiseComparisons:
+#             continue
+# #        
+#         pairwiseComparisons.append(blastKey)
+#         pairwiseComparisons.append(blastKeyRev)
+
+# print(pairwiseComparisons)
+# #        
+#         with open(f"{outputPath}/{first}-vs-{second}.tsv", 'w') as outputStream:
+#             for key, info in blastTable.items():
+#                 seqSim = info[0]
+#                 orientation = info[1]
+#                 # lots of parsing from our hashes, nothing super special
+#                 queryID = key.split(':')[0]
+#                 targetID = key.split(':')[1]
+#                 queryStrain = queryID.split('_')[0]
+#                 queryGene = int(queryID.split('_')[1])
+#                 targetStrain = targetID.split('_')[0]
+#                 targetGene = int(targetID.split('_')[1])
+#                 # if the blast-key is the same as the current strains we are looking at, then we have to write
+#                 # otherwise simply continue with the next blast hit
+#                 if strainA == queryStrain and strainB == targetStrain:
+#                     outputStream.write(f"{queryGene}\t{targetGene}\t{orientation}\t{seqSim}\n")
 
 sys.exit(0)
